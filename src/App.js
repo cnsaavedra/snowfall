2427f08b95f012346dc88bfaa266dc07fa273b1f
cnsaavedra <christian-saavedra@hotmail.com>
2018-12-07 14:26:25 -0500

    able to get snow, but not parsing string to components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Snowflake from './features/Snowflake'
+import {Stage, Layer} from 'react-konva'
+
+var snowString = "";
+
+/*
+for(var i = 0; i < 200; i++){
+  snowString += "<Snowflake />" + "\n"   
+  <React.Fragment>{snowString}</React.Fragment>
+}
+*/
+for(var i = 0; i < 200; i++){
+  React.createElement(Snowflake, null)
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App" id="fakingcanvas">
-            <div className="App-intro">
-              <Snowflake/>
-            </div>
-      </div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake /><Snowflake />
+        </Layer>
+      </Stage>
     );
   }
 }
--- src/features/Snowflake.js
+++ src/features/Snowflake.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
 //import {Circle} from 'react-shapes';
 import windowSize from 'react-window-size';
-import {Circle} from 'react-konva';
+import {Circle, Rect} from 'react-konva';
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
 
 class Snowflake extends Component{
-
     constructor(props) {
         super(props);
-        this.state = { x: Math.random() * window.innerWidth, y: Math.random() * window.innerWidth}
+        this.state = { x: Math.random() * window.innerWidth, y: Math.random() * window.innerWidth, radius: getRandomArbitrary(5, 10)}
       }
 
       handleEvent = () => {
@@ -31,16 +30,15 @@ class Snowflake extends Component{
 
       fall(){
         this.setState({  
-            x: this.state.x-1,
-            y: this.state.y-1
+            x: this.state.x,
+            y: this.state.y+1,
+            radius: this.state.radius
         })
       }
 
     render(){
         return(
-            <div className="Snowflake">
-                <Circle x = {this.state.x} y = {this.state.y} radius = {10} fill = {'white'} stroke = {'black'} strokeWidth = {4} />
-            </div>
+            <Circle x = {this.state.x} y = {this.state.y} radius = {this.state.radius} fill = {'white'} stroke = {'black'} strokeWidth = {4} />
         )
     }
 }
